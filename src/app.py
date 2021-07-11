0cab1cd977561270ea9f5bff5b89ec1b1980513f
MaheshKok <maheshkokare100@gmail.com>
2021-07-11 13:04:52 +0530

    added docs to app

--- src/app.py
+++ src/app.py
@@ -23,17 +23,22 @@ def create_webapp() -> Flask:  # pragma: no cover
     return app
 
 
-requests.get("https://vbiz.in/optionchain/foc.php?symbol=NIFTY&expiry=15JUL2021", headers=
-{
-    "accept": "*/*",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-IN,en;q=0.9,hi-IN;q=0.8,hi;q=0.7,mr-IN;q=0.6,mr;q=0.5,en-GB;q=0.4,en-US;q=0.3",
-    "origin": "http://vbiz.in",
-    "referer": "http://vbiz.in/",
 
-    "sec-ch-ua-mobile": "?0",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-"sec-fetch-site": "cross-site",
-"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"
-})
+# below code to fetch option chain data
+import requests
+
+requests.get(
+    "https://vbiz.in/optionchain/foc.php?symbol=NIFTY&expiry=15JUL2021",
+    headers={
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-IN,en;q=0.9,hi-IN;q=0.8,hi;q=0.7,mr-IN;q=0.6,mr;q=0.5,en-GB;q=0.4,en-US;q=0.3",
+        "origin": "http://vbiz.in",
+        "referer": "http://vbiz.in/",
+        "sec-ch-ua-mobile": "?0",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36",
+    },
+)
